03222de801d603a8fdb97b15ad6ceedf0a1cbf1c
Can Thang <thangcx@vnisc.com.vn>
2025-07-03 16:27:17 +0700

    delete

--- .vercel/output/_functions/pages/contact/formcontact.astro.mjs
+++
@@ -1,6 +0,0 @@
-import { _ as _page } from '../../chunks/FormContact_NXhlkG51.mjs';
-export { renderers } from '../../renderers.mjs';
-
-const page = () => _page;
-
-export { page };
--- .vercel/output/_functions/pages/home.astro.mjs
+++
@@ -1,6 +0,0 @@
-import { _ as _page } from '../chunks/index_DxME4ZXO.mjs';
-export { renderers } from '../renderers.mjs';
-
-const page = () => _page;
-
-export { page };
