4c91cf45de3d474a66a4261ebf2c01f61ad08097
Can Thang <thangcx@vnisc.com.vn>
2025-07-03 16:30:52 +0700

    sdfsdf

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,7 +1,7 @@
 import { defineConfig } from 'astro/config';
-import node from '@astrojs/node';
 import react from '@astrojs/react';
 import sitemap from '@astrojs/sitemap';
+import vercel from '@astrojs/vercel/serverless';
 
 export default defineConfig({
   site: 'https://test-gg-seven.vercel.app',
@@ -12,9 +12,7 @@ export default defineConfig({
     sitemap(),
   ],
   output: 'server',
-  adapter: node({
-    mode: 'standalone',
-  }),
+  adapter: vercel(),
   vite: {
     css: {
       preprocessorOptions: {
